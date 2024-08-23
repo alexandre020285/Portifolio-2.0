dbec877ad5a5a518f3d71715d0fb9e6b346971f5
Alexandre <alexandre1alves@gmail.com>
2024-08-23 15:54:18 -0300

    vai dar certo

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// let btnMenu = document.getElementById('btn-menu') /**/
-// let menu = document.getElementById('menu-mobile') /**/
-// let overlayMenu = document.getElementById('overlay-menu')  /**/
-
-// btnMenu.addEventListener('click', () => {   /**/
-//     menu.classList.add('abrir-menu')
-// })
-
-// menu.addEventListener('click', () => {    /**/
-//     menu.classList.remove('abrir-menu')
-// })
-// overlayMenu.addEventListener('click', () => {   /**/
-//     menuMobile.classList.remove('abrir-menu')
-// })
-
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
