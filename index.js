1d9fbf53dbb1426ff3850e25e7b91f85228d9c35
alexandre <alexandre1alves@gmail.com>
2025-03-09 20:13:48 -0300

    ajustes button

--- index.js
+++ index.js
@@ -4,30 +4,38 @@ const btnFechar = document.querySelector('.btn-fechar');
 const overlayMenu = document.getElementById('overlay-menu');
 const links = document.querySelectorAll('.menu-mobile nav ul li a');
 
+const closeMenu = () => {
+  menuMobile.classList.remove('abrir-menu');
+  setTimeout(() => {
+    btnMenu.style.display = 'flex';
+  }, 500);
+};
+
 btnMenu.addEventListener('click', () => {
   menuMobile.classList.add('abrir-menu');
   btnMenu.style.display = 'none';
 });
 
-btnFechar.addEventListener('click', () => {
-  menuMobile.classList.remove('abrir-menu');
-  setTimeout(() => {
-    btnMenu.style.display = 'flex';
-  }, 500);
-});
+btnFechar.addEventListener('click', closeMenu);
+overlayMenu.addEventListener('click', closeMenu);
+links.forEach(link => link.addEventListener('click', closeMenu));
 
-overlayMenu.addEventListener('click', () => {
-  menuMobile.classList.remove('abrir-menu');
-  setTimeout(() => {
-    btnMenu.style.display = 'flex';
-  }, 500);
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonsInLinks = document.querySelectorAll('a button');
 
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    menuMobile.classList.remove('abrir-menu');
-    setTimeout(() => {
-      btnMenu.style.display = 'flex';
-    }, 500);
+  buttonsInLinks.forEach(button => {
+    button.addEventListener('click', e => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const parentLink = button.closest('a');
+      const targetBlank = parentLink.getAttribute('target') === '_blank';
+
+      if (targetBlank) {
+        window.open(parentLink.href, '_blank', 'noopener,noreferrer');
+      } else {
+        window.location.href = parentLink.href;
+      }
+    });
   });
 });
